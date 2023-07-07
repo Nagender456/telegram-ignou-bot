ad1d2b9e984fd26df699ff1f369282e85dd2ba2a
Nagender Rajput <nagenderrajput54@gmail.com>
2023-07-07 11:12:09 +0530

    increased retry tries duration and delay

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 def configure():
-	from dotenv import load_dotenv
-	load_dotenv()
+    from dotenv import load_dotenv
+    load_dotenv()
+
 
 def main():
-	configure()
-	from messageHandler import MessageHandler
-	from telethon import TelegramClient, events
-	import os
-	messageHandler = MessageHandler()
-	teleBot = TelegramClient('bot', os.getenv('teleApiId'), os.getenv('teleApiHash')).start(bot_token=os.getenv('teleBotToken'))
+    configure()
+    from messageHandler import MessageHandler
+    from telethon import TelegramClient, events
+    import os
+    messageHandler = MessageHandler()
+    teleBot = TelegramClient('bot', os.getenv('teleApiId'), os.getenv(
+        'teleApiHash'), request_retries=100, connection_retries=100, retry_delay=5).start(bot_token=os.getenv('teleBotToken'))
 
-	@teleBot.on(events.NewMessage(incoming=True))
-	async def handleMessage(event):
-		try:
-			responses = await messageHandler.handleMessage(event)
-		except Exception as e:
-			await event.reply(f"An Error Occurred\n\n{e}")
-		for response in responses:
-			if response[0] == 0:
-				await event.respond(response[2])
-			elif response[0] == 1:
-				await event.reply(response[2])
-	teleBot.start()
-	teleBot.run_until_disconnected()
+    @teleBot.on(events.NewMessage(incoming=True))
+    async def handleMessage(event):
+        try:
+            responses = await messageHandler.handleMessage(event)
+        except Exception as e:
+            await event.reply(f"An Error Occurred\n\n{e}")
+        for response in responses:
+            if response[0] == 0:
+                await event.respond(response[2])
+            elif response[0] == 1:
+                await event.reply(response[2])
+    teleBot.start()
+    teleBot.run_until_disconnected()
+
 
 if __name__ == '__main__':
-	main()
+    main()
